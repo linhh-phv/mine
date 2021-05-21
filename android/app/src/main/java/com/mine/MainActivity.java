bc4b5d4d552bb1eb350dda8ff984f80779e9a904
linh <linh.pham@hdwebsoft.co>
2021-05-22 00:48:10 +0700

    update splash creen

--- android/app/src/main/java/com/mine/MainActivity.java
+++ android/app/src/main/java/com/mine/MainActivity.java
@@ -1,6 +1,8 @@
 package com.mine;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +14,11 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "mine";
   }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this,true);  // here 
+      super.onCreate(savedInstanceState);
+  }
+
 }
